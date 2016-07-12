e2f82ded8626c359dfc4e72bfd54085459ebf80c
Ubuntu <ubuntu@example.com>
2016-07-12 14:15:21 +0000

    Roll back

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -4,7 +4,6 @@ search('aws_opsworks_app').each do |app|
         Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
         Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
         Chef::Log.info(app)
-        Chef::Log.info("**** Name: '#{node["deploy"][0]["application"]}' ***")
     end
 
 end
