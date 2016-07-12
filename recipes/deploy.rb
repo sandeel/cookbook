260abb3b3bad97bf7eeb6f436bb5edd9169830f3
Ubuntu <ubuntu@example.com>
2016-07-12 14:02:53 +0000

    Only log if app is being deployed

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,7 +1,9 @@
 search('aws_opsworks_app').each do |app|
 
-  Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
-  Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
-  Chef::Log.info(app)
+    if app['deploy'] == true
+        Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+        Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+        Chef::Log.info(app)
+    end
 
 end
